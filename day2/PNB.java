0e4efdade4a11c13f54a9587481423c984356254
SasikumarSasiKumar <99641406+SasikumarSasiKumar@users.noreply.github.com>
2022-02-22 21:26:48 +0530

    Add files via upload

---
+++ day2/PNB.java
@@ -0,0 +1,11 @@
+package week3.day2;
+
+public interface PNB {
+	
+	public void MaximumLoanAmount();
+	
+	public void CreditScore();
+	
+	public void minimumBalance();
+
+}
---
+++ day2/SBI.java
@@ -0,0 +1,17 @@
+package week3.day2;
+
+public abstract  class SBI {
+	
+	public void itlone() {  // implemented method
+		
+	}
+
+	public abstract void  bankBalance();   // unimplement method
+		
+	
+	public  abstract void maximumloanaAmount();
+	
+	
+	
+}
+	
