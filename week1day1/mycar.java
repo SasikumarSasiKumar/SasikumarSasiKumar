b5cfc615b6a696ec646bd05800f0b99e579f1553
SasikumarSasiKumar <99641406+SasikumarSasiKumar@users.noreply.github.com>
2022-03-02 01:23:54 +0530

    Add files via upload

---
+++ week1day1/mycar.java
@@ -0,0 +1,25 @@
+package week.week1day1;
+
+public class mycar {
+
+	public void applyBreak() {
+		System.out.println("apply Break");
+	}
+	public void applyGear(){
+		System.out.println("apply gear");
+	}
+	public void switchOnac() {
+		System.out.println("switchOn ac");
+	}
+		
+public static void main(String[] args) {
+	mycar Car= new mycar ();
+	Car. applyBreak();
+	Car. applyGear();
+	Car. switchOnac();
+	
+	
+}
+	}
+
+
